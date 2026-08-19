033ef4ba41c1092ed3e6a4f2d7613a85ae8b096e
spandana56321-art <spandana56321@gmail.com>
2026-08-19 11:29:19 +0530

    Update Nodemailer transporter configuration

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,7 +9,10 @@ const Otp = require("../models/Otp");
 // ====================================
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
